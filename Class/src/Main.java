d592e18de4573b9a0e3368441b5fffc2a066af92
JoséJ <JosJ@PCname>
2018-08-27 16:13:10 -0400

    Cheking on stuff

---
+++ Class/src/Main.java
@@ -0,0 +1,7 @@
+
+public class Main {
+	public static void main(String[] args) {
+		Triangle theTriangle1 = new Triangle(10.0, 10.0, 10,0);
+		double area = theTriangle1.areaOfTriangle();
+	}
+}
---
+++ Class/src/Triangle.java
@@ -0,0 +1,17 @@
+
+public class Triangle {
+	 public double side1;
+	 public double side2;
+	 public double side3;
+	 
+	 public Triangle(double s1, double s2, double s3) {
+		 side1 = s1;
+		 side2 = s2;
+		 side3 = s3;
+	 }
+	 public static double areaOfTriangle() {
+		 double s = (this.side1 + this.side2 + this.side3)/2.0;
+		 double area = Math.sqrt(s*(s- this.side1)*(s - this.side2)*(s - this.side3));
+		 return area;
+	 }
+}
